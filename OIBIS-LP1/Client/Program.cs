a7e4e1daec741cfac8fc523c442cc337461d800d
Milan Sremcevic <kdusan693@gmail.com>
2023-11-06 18:22:28 +0100

    kontrolna tacka 2

--- OIBIS-LP1/Client/Program.cs
+++ OIBIS-LP1/Client/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,13 +13,18 @@ namespace Client
     {
         static void Main(string[] args)
         {
-            ChannelFactory<ITest> channel = new ChannelFactory<ITest>("Test");
 
-            ITest proxy = channel.CreateChannel();
+            X509Certificate2 clientCertificate = LoadClientCertificate(); // Implementirajte funkciju za učitavanje klijentskog sertifikata
+
+            ChannelFactory<ITest> factory = new ChannelFactory<ITest>(new WSHttpBinding());
+            factory.Credentials.ClientCertificate.Certificate = clientCertificate;
+
+
+            ITest proxy = factory.CreateChannel();
 
             proxy.Test();
 
-            Console.WriteLine("Client. " + channel.Endpoint.Address);
+            Console.WriteLine("Client. " + factory.Endpoint.Address);
 
             Console.ReadLine();
 
